1ad951638cd7872bf551fd7df747dfd45cfec182
Oliver Klingefjord <oliverklingefjord@gmail.com>
2022-12-05 22:51:17 +0100

    asyncify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import typing
 import telegram
 import logging
@@ -42,19 +43,19 @@ async def send(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # get the user's browser instance, or create one if it doesn't exist
     if update.effective_user.username not in browsers:
         await update.message.reply_text("Hang on, setting up your assistant...")
-        browser = Browser()
+
+        browser = Browser(update.effective_user.id)
         browsers[update.effective_user.id] = browser
-        browser.login()
+
+        await browser.connect()
+        await browser.login()
 
     # get the response from the API
-    browser.send_message(update.message.text)
-
-    async def typing_func():
-        await application.bot.send_chat_action(update.effective_chat.id, "typing")
+    await browser.send_message(update.message.text)
 
     # wait for the response to load
-    await browser.check_loading(update, typing_func)
-    response = browser.get_last_message()
+    await check_loading(update, browser)
+    response = await browser.get_last_message()
 
     # send the response to the user
     await update.message.reply_text(
@@ -67,14 +68,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
 
     if update.effective_user.username in browsers:
-        await update.message.reply_text("Setting up your assistant...")
+        await update.message.reply_text("You already have an assistant. Use /reset to reset your assistant.")
         return
 
     await update.message.reply_text("Getting ready...")
+
     browser = Browser(update.effective_user.id)
-    browser.login()
     browsers[update.effective_user.id] = browser
 
+    await browser.connect()
+    await browser.login()
     await update.message.reply_text("You are ready to start using Lydia. Say hello!")
 
 @auth()
@@ -83,18 +86,39 @@ async def reset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     if update.effective_user.username in browsers:
         update.message.reply_text("Resetting your assistant...")
-        browsers[update.effective_user.username].login()
+        await browsers[update.effective_user.username].login()
         await update.message.reply_text("You are ready to start using Lydia. Say hello!")
     else:
         await update.message.reply_text("You don't have an assistant yet. Use /start to get started.")
 
 
+async def check_loading(update, browser):
+    # with a timeout of 90 seconds, created a while loop that checks if loading is done
+    loading = await browser.page.query_selector_all(
+        "button[class^='PromptTextarea__PositionSubmit']>.text-2xl"
+    )
+    # keep checking len(loading) until it's empty or 45 seconds have passed
+    await application.bot.send_chat_action(update.effective_chat.id, "typing")
+    start_time = time.time()
+    while len(loading) > 0:
+        if time.time() - start_time > 90:
+            break
+        time.sleep(0.5)
+        loading = await browser.page.query_selector_all(
+            "button[class^='PromptTextarea__PositionSubmit']>.text-2xl"
+        )
+        await application.bot.send_chat_action(update.effective_chat.id, "typing")
+
+async def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update cause error "%s"', context.error)
 
 def main():
     # Handle messages    
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, send))
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("reload", reset))
+    application.add_error_handler(error)
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
--- modules/browser.py
+++ modules/browser.py
@@ -1,7 +1,8 @@
 
 import os
 import time
-from playwright.sync_api import sync_playwright
+from anyio import sleep
+from playwright.async_api import async_playwright
 from telegram import Update
 from telegram.helpers import escape, escape_markdown
 
@@ -9,32 +10,36 @@ class Browser:
 
     def __init__(self, user_id: str) -> None:
         """Create a browser instance for a user"""
-        play = sync_playwright().start()
-        browser = play.chromium.launch_persistent_context(
-            user_data_dir="/tmp/playwright",
-            headless=True,
-        )
-
-        self.page = browser.new_page()
         self.user_id = user_id
 
+    async def connect(self):
+        """Connect to the webpage"""
 
-    def get_last_message(self):
+        play = await async_playwright().start()
+        context = await play.chromium.launch_persistent_context(
+            user_data_dir="/tmp/playwright",
+            headless=False,
+        )
+
+        self.page = await context.new_page()
+
+
+    async def get_last_message(self):
         """Get the latest message"""
-        page_elements = self.page.query_selector_all("div[class*='ConversationItem__Message']")
+        page_elements = await self.page.query_selector_all("div[class*='ConversationItem__Message']")
         last_element = page_elements[-1]
         prose = last_element.query_selector(".prose")
         try:
-            code_blocks = prose.query_selector_all("pre")
+            code_blocks = await prose.query_selector_all("pre")
         except Exception as e:
             response = "Server probably disconnected, try running /reset"
         if len(code_blocks) > 0:
             # get all children of prose and add them one by one to respons
             response = ""
-            for child in prose.query_selector_all("p,pre"):
+            for child in await prose.query_selector_all("p,pre"):
                 print(child.get_property("tagName"))
                 if str(child.get_property("tagName")) == "PRE":
-                    code_container = child.query_selector(
+                    code_container = await child.query_selector(
                         "div[class*='CodeSnippet__CodeContainer']"
                     )
                     response += f"\n```\n{escape_markdown(code_container.inner_text(), version=2)}\n```"
@@ -49,66 +54,48 @@ class Browser:
         return response
 
      
-    def __get_input_box(self):
+    async def __get_input_box(self):
         """Get the child textarea of `PromptTextarea__TextareaWrapper`"""
-        return self.page.query_selector("textarea")
+        return await self.page.query_selector("textarea")
 
-    def login(self, attempt=0):
+    async def login(self, attempt=0):
         """Log in to OpenAI Chat. Try 3 times before giving up"""
         if attempt > 2:
             raise Exception("Failed to login")
 
         # check if we're already logged in
-        self.page.goto("https://chat.openai.com/")
-        if self.__get_input_box() is not None:
+        await self.page.goto("https://chat.openai.com/")
+        if await self.__get_input_box() is not None:
             print("Already logged in")
             return
         
         print("Logging in")
 
         # click on the login button        
-        self.page.locator("button", has_text='Log in').click()
-        time.sleep(3)
+        await self.page.locator("button", has_text='Log in').click()
+        await sleep(1)
 
-        save = self.page.locator("button[value='default']", has_text='Continue')
+        save = await self.page.locator("button[value='default']", has_text='Continue')
 
         # fill in the email
-        email = self.page.locator("input[id='username']")
-        email.fill(os.getenv("OPEN_AI_USERNAME"))
-        save.click()
-        time.sleep(1)
+        email = await self.page.locator("input[id='username']")
+        await email.fill(os.getenv("OPEN_AI_USERNAME"))
+        await save.click()
+        await sleep(1)
 
         # fill in the password
-        password = self.page.locator("input[id='password']")
-        password.fill(os.getenv("OPEN_AI_PASSWORD"))
-        save.click()
-        time.sleep(5)
+        password = await self.page.locator("input[id='password']")
+        await password.fill(os.getenv("OPEN_AI_PASSWORD"))
+        await save.click()
+        await time.sleep(5)
 
         # the user should be logged in now. Otherwise, try again.
         if self.__get_input_box() is None:
             return self.login(attempt=attempt+1)
 
-
-    async def check_loading(self, update: Update, typing_func: callable):
-        # with a timeout of 90 seconds, created a while loop that checks if loading is done
-        loading = self.page.query_selector_all(
-            "button[class^='PromptTextarea__PositionSubmit']>.text-2xl"
-        )
-        # keep checking len(loading) until it's empty or 45 seconds have passed
-        await typing_func()
-        start_time = time.time()
-        while len(loading) > 0:
-            if time.time() - start_time > 90:
-                break
-            time.sleep(0.5)
-            loading = self.page.query_selector_all(
-                "button[class^='PromptTextarea__PositionSubmit']>.text-2xl"
-            )
-            await typing_func()
-
-    def send_message(self, message):
+    async def send_message(self, message):
         """Send a message to the webpage"""
-        box = self.__get_input_box()
-        box.click()
-        box.fill(message)
-        box.press("Enter")
+        box = await self.__get_input_box()
+        await box.click()
+        await box.fill(message)
+        await box.press("Enter")
