d810da5c29940dd31a6b2027606a7ed4e8627f0c
Oliver Klingefjord <oliverklingefjord@gmail.com>
2022-12-05 22:19:44 +0100

    barebones

--- modules/browser.py
+++ modules/browser.py
@@ -1,16 +1,23 @@
 
+import os
+import time
 from playwright.sync_api import sync_playwright
+from telegram import Update
 from telegram.helpers import escape, escape_markdown
 
 class Browser:
-    def __init__(self) -> None:
-        # Open the webpage
+
+    def __init__(self, user_id: str) -> None:
+        """Create a browser instance for a user"""
         play = sync_playwright().start()
         browser = play.chromium.launch_persistent_context(
             user_data_dir="/tmp/playwright",
-            headless=False,
+            headless=True,
         )
+
         self.page = browser.new_page()
+        self.user_id = user_id
+
 
     def get_last_message(self):
         """Get the latest message"""
@@ -19,8 +26,8 @@ class Browser:
         prose = last_element.query_selector(".prose")
         try:
             code_blocks = prose.query_selector_all("pre")
-        except AttributeError as e:
-            response = "Server probably disconnected, try running /reload"
+        except Exception as e:
+            response = "Server probably disconnected, try running /reset"
         if len(code_blocks) > 0:
             # get all children of prose and add them one by one to respons
             response = ""
@@ -32,7 +39,7 @@ class Browser:
                     )
                     response += f"\n```\n{escape_markdown(code_container.inner_text(), version=2)}\n```"
                 else:
-                    # replace all <code>x</code> things with `x`
+                    # replace <code></code> formatting with ``
                     text = child.inner_html()
                     response += escape_markdown(text, version=2)
             response = response.replace("<code\>", "`")
@@ -42,17 +49,66 @@ class Browser:
         return response
 
      
-    def get_input_box(self):
+    def __get_input_box(self):
         """Get the child textarea of `PromptTextarea__TextareaWrapper`"""
         return self.page.query_selector("textarea")
 
-    def is_logged_in(self):
-        """See if we have a textarea with data-id='root'"""
-        return self.get_input_box() is not None
+    def login(self, attempt=0):
+        """Log in to OpenAI Chat. Try 3 times before giving up"""
+        if attempt > 2:
+            raise Exception("Failed to login")
+
+        # check if we're already logged in
+        self.page.goto("https://chat.openai.com/")
+        if self.__get_input_box() is not None:
+            print("Already logged in")
+            return
+        
+        print("Logging in")
+
+        # click on the login button        
+        self.page.locator("button", has_text='Log in').click()
+        time.sleep(3)
+
+        save = self.page.locator("button[value='default']", has_text='Continue')
+
+        # fill in the email
+        email = self.page.locator("input[id='username']")
+        email.fill(os.getenv("OPEN_AI_USERNAME"))
+        save.click()
+        time.sleep(1)
+
+        # fill in the password
+        password = self.page.locator("input[id='password']")
+        password.fill(os.getenv("OPEN_AI_PASSWORD"))
+        save.click()
+        time.sleep(5)
+
+        # the user should be logged in now. Otherwise, try again.
+        if self.__get_input_box() is None:
+            return self.login(attempt=attempt+1)
+
+
+    async def check_loading(self, update: Update, typing_func: callable):
+        # with a timeout of 90 seconds, created a while loop that checks if loading is done
+        loading = self.page.query_selector_all(
+            "button[class^='PromptTextarea__PositionSubmit']>.text-2xl"
+        )
+        # keep checking len(loading) until it's empty or 45 seconds have passed
+        await typing_func()
+        start_time = time.time()
+        while len(loading) > 0:
+            if time.time() - start_time > 90:
+                break
+            time.sleep(0.5)
+            loading = self.page.query_selector_all(
+                "button[class^='PromptTextarea__PositionSubmit']>.text-2xl"
+            )
+            await typing_func()
 
     def send_message(self, message):
         """Send a message to the webpage"""
-        box = self.get_input_box()
+        box = self.__get_input_box()
         box.click()
         box.fill(message)
         box.press("Enter")
--- utils/auth.py
+++ utils/auth.py
@@ -1,16 +1,18 @@
 
 from functools import wraps
+import dotenv
 import os
 
-if os.environ.get("TELEGRAM_USER_ID"):
-    USER_ID = int(os.environ.get("TELEGRAM_USER_ID"))
+dotenv.load_dotenv()
+
+allowed_users = os.getenv("ALLOWED_USERS").split(",")
 
 def auth():
     """Verify that the user is allowed to use the bot."""
     def decorator(func: callable):
         @wraps(func)
         async def wrapper(update, context):
-            if update.effective_user.id == USER_ID:
+            if update.effective_user.username in allowed_users:
                 await func(update, context)
             else:
                 await update.message.reply_text(
